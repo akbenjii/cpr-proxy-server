5e10bebb77ae5ef91f34952416f301549ed3ac18
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2021-02-07 11:40:50 -0500

    Update message.js

--- src/events/world/message.js
+++ src/events/world/message.js
@@ -1,36 +1,67 @@
 'use strict'
 
+const fs = require('fs');
+const { pack, unpack } = require('msgpackr');
+
 const cryptoKey = require('../../handlers/cryptokey');
 
-const {unpack} = require('msgpackr');
+const prefix = '!';
+const commands = {};
+
+for (let file of fs.readdirSync(`${__dirname}\\..\\..\\commands`).filter(file => file.endsWith('.js'))) {
+	let command = require(`${__dirname}\\..\\..\\commands\\${file}`);
+	commands[file.slice(0, -3)] = command;
+}
 
 exports.incoming = async (penguin, data) => {
     try {
         if (penguin.key_exist === false) { // check if login:u world key exists in redis.
             penguin.client.close();
+            penguin.server.close();
+
             return logger.error(`login:u does not exists for Penguin${penguin.ip_addr}`);
         }
-        let incoming_packet = penguin.cryptoUtils.key ? unpack(await penguin.cryptoUtils.decrypt(data)) : unpack(new Uint8Array(data));
-        incoming_packet = JSON.stringify(incoming_packet);
 
-        logger.incoming(incoming_packet);
+        let incomingPacket = penguin.cryptoUtils.key ? unpack(await penguin.cryptoUtils.decrypt(data)) : unpack(data);
+        incomingPacket = JSON.stringify(incomingPacket);
+
+        logger.incoming(incomingPacket);
         penguin.client.send(data)
     } catch (err) {
         logger.error(`Client : ${err}`);
     }
 }
 
-exports.outgoing = async (penguin, data) => {
+exports.outgoing = async (penguin, data, isCustomPacket) => {
     try {
-        if (penguin.server && penguin.server.readyState !== 1) await penguin.waitFor(_ => penguin.server.readyState === 1);
+        if (penguin.server.readyState !== 1) await penguin.waitFor(_ => penguin.server.readyState === 1);
         if (!penguin.cryptoUtils['key']) await cryptoKey.init(penguin);
 
-        let outgoing_packet = penguin.cryptoUtils.key ? unpack(await penguin.cryptoUtils.decrypt(data)) : unpack(new Uint8Array(data));
-        outgoing_packet = JSON.stringify(outgoing_packet);
+        if (isCustomPacket) {
+            data = penguin.cryptoUtils.key ? await penguin.cryptoUtils.encrypt(data) : pack(data);
+        }
 
-        logger.outgoing(outgoing_packet);
+        let outgoingPacket = penguin.cryptoUtils.key ? unpack(await penguin.cryptoUtils.decrypt(data)) : unpack(data);
+
+        if (outgoingPacket.action == 'player:message') {
+            let msg = outgoingPacket.params[0].toLowerCase();
+            if (msg.startsWith(prefix)) {
+                let args = msg.split(' ');
+                let command = commands[args[0].slice(prefix.length)];
+                if (command) {
+                    args.shift();
+                    try {
+                        command(this, penguin, args);
+                    } catch(err) {
+                        logger.warn(`Command Error: ${err}`);
+                    }
+                }
+            }
+        }
+
+        logger.outgoing(JSON.stringify(outgoingPacket));
         penguin.server.send(data);
-    } catch (err) {
+    } catch(err) {
         logger.error(`Server : ${err}`);
         if (penguin.client) penguin.client.close();
     }
