ddb615033c2442f1aadd914fcc716cf75e3ce909
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2021-02-07 11:41:30 -0500

    Update message.js

--- src/events/login/message.js
+++ src/events/login/message.js
@@ -1,20 +1,19 @@
 'use strict'
 
+const { unpack } = require('msgpackr');
+
 const cryptoKey = require('../../handlers/cryptokey');
 
-const {unpack} = require('msgpackr');
-const {CHAT_REGEX} = require('../../../config');
+const { CHAT_REGEX } = require('../../../config');
 
 exports.incoming = async (login, data) => {
     try {
         let incoming_packet = login.cryptoUtils.key ? unpack(await login.cryptoUtils.decrypt(data)) : unpack(new Uint8Array(data));
         if (!login.cryptoUtils['key']) await cryptoKey.init(login, incoming_packet);
 
-        logger.incoming(JSON.stringify(incoming_packet));
-
         if (incoming_packet.action === ActionType.LOGIN.USER) {
             login._redis.exists(`login:u${login.ip_addr}`, (err, exists) => {
-                if(exists === 1) login._redis.del(`login:u${login.ip_addr}`);
+                if (exists === 1) login._redis.del(`login:u${login.ip_addr}`);
                 login._redis.set(`login:u${login.ip_addr}`, JSON.stringify(incoming_packet), login._redis.print);
             });
 
@@ -22,22 +21,24 @@ exports.incoming = async (login, data) => {
             data = await login.cryptoUtils.encrypt(incoming_packet);
         } else if (incoming_packet.action === ActionType.ENGINE.PROMPT) logger.info(`User${login.ip_addr} failed login.`);
 
+        logger.incoming(JSON.stringify(incoming_packet));
+
         login.client.send(data);
-    } catch (err) {
+    } catch(err) {
         logger.error(`Client : ${err}`)
     }
 }
 
 exports.outgoing = async (login, data) => {
     try {
-        let outgoing_packet = login.cryptoUtils.key ? unpack(await login.cryptoUtils.decrypt(data)) : unpack(new Uint8Array(data));
+        let outgoing_packet = login.cryptoUtils.key ? unpack(await login.cryptoUtils.decrypt(data)) : unpack(data);
         if (login.server && login.server.readyState !== 1) await login.waitFor(_ => login.server.readyState === 1);
 
         outgoing_packet = JSON.stringify(outgoing_packet);
         logger.outgoing(outgoing_packet);
 
         login.server.send(data);
-    } catch (err) {
+    } catch(err) {
         logger.error(`Server : ${err}`);
     }
 }
